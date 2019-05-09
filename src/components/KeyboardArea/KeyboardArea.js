b8cc2c8645fcb5cfce03fa96cefee3ceae4579f0
Green Corduroy <steve@nevizen.com>
2019-05-09 09:46:24 -0400

    Added keypress event listening

--- src/components/KeyboardArea/KeyboardArea.js
+++ src/components/KeyboardArea/KeyboardArea.js
@@ -1,61 +1,73 @@
-import React from "react";
+import React, { Component } from "react";
 
 import "./KeyboardArea.css";
 
 import Letter from "../Letter/Letter";
-// import logic from "../../utils/game";
 
 const keys = "QWERTYUIOPASDFGHJKLZXCVBNM";
 
 const letterStatus = (letter, arr) => {
-  console.log(
-    "letterStatus:",
-    arr.indexOf(letter) > -1 ? "unavailable" : "available"
-  );
   return arr.indexOf(letter) > -1 ? "unavailable" : "available";
 };
+class KeyboardArea extends Component {
+  constructor(props) {
+    super(props);
+    this.onLetterPress = this.onLetterPress.bind(this);
+  }
+  onLetterPress(e) {
+    this.props.onClick(e.key.toUpperCase());
+  }
+  componentDidMount() {
+    document.addEventListener("keypress", this.onLetterPress, false);
+  }
+  componentWillUnmount() {
+    document.removeEventListener("keypress", this.onLetterPress, false);
+  }
+  render() {
+    return (
+      <section className="keyboard">
+        <div className="keyboard-row keyboard-1">
+          {keys.split("", 10).map(letter => (
+            <Letter
+              key={letter}
+              letter={letter}
+              letterValue={letter}
+              status={letterStatus(letter, this.props.selectedLetters)}
+              onClick={() => this.props.onClick(letter)}
+            />
+          ))}
+        </div>
+        <div className="keyboard-row keyboard-2">
+          {keys
+            .slice(10, keys.length)
+            .split("", 9)
+            .map(letter => (
+              <Letter
+                key={letter}
+                letter={letter}
+                letterValue={letter}
+                status={letterStatus(letter, this.props.selectedLetters)}
+                onClick={() => this.props.onClick(letter)}
+              />
+            ))}
+        </div>
+        <div className="keyboard-row keyboard-3">
+          {keys
+            .slice(19, keys.length)
+            .split("", 7)
+            .map(letter => (
+              <Letter
+                key={letter}
+                letter={letter}
+                letterValue={letter}
+                status={letterStatus(letter, this.props.selectedLetters)}
+                onClick={() => this.props.onClick(letter)}
+              />
+            ))}
+        </div>
+      </section>
+    );
+  }
+}
 
-const KeyboardArea = props => (
-  <section className="keyboard">
-    <div className="keyboard-row keyboard-1">
-      {keys.split("", 10).map(letter => (
-        <Letter
-          key={letter}
-          letter={letter}
-          letterValue={letter}
-          status={letterStatus(letter, props.selectedLetters)}
-          onClick={() => props.onClick(letter)}
-        />
-      ))}
-    </div>
-    <div className="keyboard-row keyboard-2">
-      {keys
-        .slice(10, keys.length)
-        .split("", 9)
-        .map(letter => (
-          <Letter
-            key={letter}
-            letter={letter}
-            letterValue={letter}
-            status={letterStatus(letter, props.selectedLetters)}
-            onClick={() => props.onClick(letter)}
-          />
-        ))}
-    </div>
-    <div className="keyboard-row keyboard-3">
-      {keys
-        .slice(19, keys.length)
-        .split("", 7)
-        .map(letter => (
-          <Letter
-            key={letter}
-            letter={letter}
-            letterValue={letter}
-            status={letterStatus(letter, props.selectedLetters)}
-            onClick={() => props.onClick(letter)}
-          />
-        ))}
-    </div>
-  </section>
-);
 export default KeyboardArea;
