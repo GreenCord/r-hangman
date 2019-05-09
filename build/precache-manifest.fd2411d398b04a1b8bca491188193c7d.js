8499b219568625785aec3204bfd9dad9e171f261
Green Corduroy <steve@nevizen.com>
2019-05-09 08:02:43 -0400

    Deployment build

---
+++ build/precache-manifest.fd2411d398b04a1b8bca491188193c7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d37159565ef682fe7327dc603c517420",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15db8a05dce8942b0d73",
+    "url": "/static/css/main.80759546.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "15db8a05dce8942b0d73",
+    "url": "/static/js/main.c768bf30.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d846fe7facdf937851dc87662574b056.js"
+  "/precache-manifest.fd2411d398b04a1b8bca491188193c7d.js"
 );
 
 self.addEventListener('message', (event) => {
